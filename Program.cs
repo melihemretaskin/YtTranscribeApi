594d7639ab950e6ea4e7092ef57abd4994030ebd
Melih Emre Taşkın <melihemretaskin@outlook.com>
2026-01-01 17:13:59 +0300

    afxs

--- Program.cs
+++ Program.cs
@@ -1,11 +1,17 @@
 ﻿using Microsoft.AspNetCore.Http.Features;
 using Microsoft.AspNetCore.HttpOverrides;
+using Microsoft.OpenApi;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(); // ❗ filtre yok
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "YtTranscribeApi", Version = "v1" });
+});
+
 builder.Services.AddHttpClient();
 
 builder.Services.Configure<FormOptions>(o =>
@@ -26,7 +32,10 @@ app.UseForwardedHeaders(new ForwardedHeadersOptions
 });
 
 app.UseSwagger();
-app.UseSwaggerUI();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "YtTranscribeApi v1");
+});
 
 app.MapControllers();
 app.Run();
